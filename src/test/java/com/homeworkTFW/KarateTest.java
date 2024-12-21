9e4a4169aee674621ce3f5463833d543f7bec064
cSora <72693167+cSora@users.noreply.github.com>
2024-12-21 21:48:52 +0000

    relocate feature files and fix runner

--- src/test/java/com/homeworkTFW/KarateTest.java
+++ src/test/java/com/homeworkTFW/KarateTest.java
@@ -2,9 +2,11 @@ package com.homeworkTFW;
 
 import com.intuit.karate.junit5.Karate;
 
-class KarateTestRunner {
+class KarateTest {
+
     @Karate.Test
     Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+        return Karate.run("features/example").relativeTo(getClass());
     }
+
 }
